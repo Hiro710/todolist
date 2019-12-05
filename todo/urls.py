77d58bc4b20c36f60b2fe3c9e0bbd0bd427bbc48
Toropon <duck7quick@gmail.com>
2019-12-05 15:19:45 +0900

    add detail.html DetailView

--- todo/urls.py
+++ todo/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 # 呼び出す際のファイル場所を指示する
-from .views import TodoList
+from .views import TodoList, TodoDetail
 
 urlpatterns = [
     path('list/', TodoList.as_view()),
+    # <int:pk>でデータの各ID(primary_key)を持ってくる(指定してあげる)
+    path('detail/<int:pk>', TodoDetail.as_view()),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 # TodoModelが入っている場所を指示する(ファイルのパス)
 from .models import TodoModel
 
+# ListViewはListを表示するための機能が付いている
 class TodoList(ListView):
     template_name = 'list.html'
     # DBのどのモデルを使うのかを指定する
+    model = TodoModel
+
+# DetailViewはpkというものをURLに付けることによって、
+# 一個一個のデータを取得してそれを表示する機能が付いている
+class TodoDetail(DetailView):
+    template_name = 'detail.html'
     model = TodoModel
