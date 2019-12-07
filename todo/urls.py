34ce12d2dd1652679015b6dfc7281da2c985a0f0
Toropon <duck7quick@gmail.com>
2019-12-07 11:22:18 +0900

    fix CreateView

--- todo/urls.py
+++ todo/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-# 呼び出す際のファイル場所を指示する
-from .views import TodoList, TodoDetail
+# 呼び出す際のファイル場所を指示する(views.pyファイル内)
+from .views import TodoList, TodoDetail, TodoCreate
 
 urlpatterns = [
-    path('list/', TodoList.as_view()),
+    # nameでurlパターンに応じてviewに名前を付ける(関連付け)
+    path('list/', TodoList.as_view(), name = 'list'),
     # <int:pk>でデータの各ID(primary_key)を持ってくる(指定してあげる)
-    path('detail/<int:pk>', TodoDetail.as_view()),
+    path('detail/<int:pk>', TodoDetail.as_view(), name = 'detail'),
+    path('create/', TodoCreate.as_view(), name = 'create'),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
 # TodoModelが入っている場所を指示する(ファイルのパス)
 from .models import TodoModel
+# reverse_lazy('')が入っている場所を指示する
+from django.urls import reverse_lazy
 
 # ListViewはListを表示するための機能が付いている
 class TodoList(ListView):
@@ -14,3 +16,13 @@ class TodoList(ListView):
 class TodoDetail(DetailView):
     template_name = 'detail.html'
     model = TodoModel
+
+class TodoCreate(CreateView):
+    template_name = 'create.html'
+    model = TodoModel
+    # formでフォームを表示する際はfieldsを使ってどのモデルのデータを持ってくるのか指定する
+    fields = ('title', 'memo', 'priority', 'duedate')
+    # データの作成が完了した時のリダイレクト先を指定する(必須)
+    # reverse_lazy('')はclassの中で指定する際に使用する
+    # urls.pyでnameを使って指定したものをviewと関連付ける
+    success_url = reverse_lazy('list')
